15b4464e7807aa90df5910b633f3f4d07439f245
alinojomi <anojomi3@gmail.com>
2023-09-26 09:20:18 +0330

    Added fetchIpFunction for fetching ip of the user

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { useState, useEffect } from "react";
+import dotenv from "dotenv";
 import style from "./App.module.css";
 import { TbMapPinCode } from "react-icons/tb";
 
+dotenv.config();
+
 function App() {
+  const [fetchIpData, fetchIpDataHandler] = useState({});
+
+  function fetchIpFunction() {
+    try {
+      fetch(`https://ipinfo.io?token=${process.env.ACCESS_TOKEN}`)
+        .then((response) => response.json())
+        .then((data) => fetchIpData(data))
+        .catch((error) => console.error(error));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchIpFunction();
+  });
+
   return (
     <section className={style["container"]}>
       <div className={style["icon-container"]}>
