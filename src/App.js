354d4953656d6c4c10c8229dc4abd95ee0ccf28d
alinojomi <anojomi3@gmail.com>
2023-09-26 09:01:25 +0330

    Added icon with css file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import logo from "./logo.svg";
-import "./App.css";
+import style from "./App.module.css";
+import { TbMapPinCode } from "react-icons/tb";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className={style["container"]}>
+      <div className={style["icon-container"]}>
+        <div className={style["icon"]}>
+          <TbMapPinCode />
+        </div>
+      </div>
+      <div className={style["info-container"]}></div>
+    </section>
   );
 }
 
