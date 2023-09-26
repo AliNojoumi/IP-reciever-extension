b67d233b13585a1d2ac9d6bacbdc8b01ae938fef
alinojomi <anojomi3@gmail.com>
2023-09-26 09:45:17 +0330

    Fixed infinite api requests

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { useState, useEffect } from "react";
-import dotenv from "dotenv";
 import style from "./App.module.css";
 import { TbMapPinCode } from "react-icons/tb";
-
-dotenv.config();
 
 function App() {
   const [fetchIpData, fetchIpDataHandler] = useState({});
 
   function fetchIpFunction() {
     try {
-      fetch(`https://ipinfo.io?token=${process.env.ACCESS_TOKEN}`)
+      fetch(`https://ipinfo.io?token=54e3c02870b938`)
         .then((response) => response.json())
-        .then((data) => fetchIpData(data))
+        .then((data) => {
+          fetchIpDataHandler(data);
+          console.log(data);
+        })
         .catch((error) => console.error(error));
     } catch (error) {
       console.error(error);
@@ -21,7 +21,7 @@ function App() {
 
   useEffect(() => {
     fetchIpFunction();
-  });
+  }, []);
 
   return (
     <section className={style["container"]}>
